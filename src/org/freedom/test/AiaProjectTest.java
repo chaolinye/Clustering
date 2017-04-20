e33be8c6556b3448359ea1422659c81fc72ece4d
yechaolin <2285915269@qq.com>
2017-04-20 19:53:17 +0800

    complete aia analyze

--- src/org/freedom/test/AiaProjectTest.java
+++ src/org/freedom/test/AiaProjectTest.java
@@ -1,7 +1,18 @@
 package org.freedom.test;
 
+import org.freedom.cluster.AiaProject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * Created by chaolin on 2017/4/20.
  */
 public class AiaProjectTest {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file=new File("D:\\test\\standard\\a4353.aia");
+        AiaProject aia=new AiaProject(file);
+        System.out.println("AiaProject.isValid: "+aia.isValid());
+        System.out.println("mutations: "+aia.getMutationCounter());
+    }
 }
